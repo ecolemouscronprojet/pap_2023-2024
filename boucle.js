4cfdc6a6e7d77de4b519dbfb8aa4fac6d43c533b
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-10-17 19:00:41 +0200

    wip

--- boucle.js
+++ boucle.js
@@ -15,10 +15,26 @@
 
 // EXERCIC 3
 // AFFICHE LA TABLE DE MULTIPLICATION DE 5
-const nb = 6
-for(let i=1; i<=10; i++) {
-    console.log(`${i}X ${nb} = ${i*nb}`)
+// console.time()
+// const nb = 6
+// for(let i=1; i<=10000000; i++) {
+//     console.log(`${i}X ${nb} = ${i*nb}`)
+// }
+// console.timeEnd()
+
+// EXERCICE 4
+
+// Affichez les nombres de 1 à 20
+// sur les nombres divisibles par trois rajoutées un message à côté nombre divisible par trois
+
+for(let i=1; i<=20; i++) {
+    console.log(`${i} ${i % 3 == 0 ? "nombre divisible par 3" : ""}`)
 }
+
+
+
+
+
 
 
 
