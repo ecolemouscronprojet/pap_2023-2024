bef4e2d657992883d7605c341a64c306268b98b4
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-10-17 18:36:03 +0200

    wip

---
+++ boucle.js
@@ -0,0 +1,25 @@
+// // EXERCICE 1 
+// for(let i=1; i<=10; i++) {
+//     console.log(`Hello world ${i}`)
+// }
+
+
+// EXERCICE 2
+// AFFICHER LES NOMBRES SUIVANTS
+// 2 4 6 8 10 12
+
+// for(let i=2; i<=12; i+=2) {
+//     console.log(`Hello world ${i}`)
+// }
+
+
+// EXERCIC 3
+// AFFICHE LA TABLE DE MULTIPLICATION DE 5
+const nb = 6
+for(let i=1; i<=10; i++) {
+    console.log(`${i}X ${nb} = ${i*nb}`)
+}
+
+
+
+
---
+++ exercice_tableau.js
@@ -0,0 +1,23 @@
+const t1 = [ 'cerise', 'poire', 'orange', 'pomme']
+
+// cerise doit se retrouver à la position 2 OK
+// poire à la position 3
+// orange à la position 0 OK
+// pomme à la position 1
+
+// let tmp = t1[2]
+// t1[2] = t1[0];
+// t1[0] = tmp;
+// tmp = t1[1]
+// t1[1] = t1[3]
+// t1[3] = tmp;
+
+
+
+// [ 'orange', 'pomme', 'cerise', 'poire' ]
+
+
+
+
+// console.log(tmp)
+console.log(t1)
